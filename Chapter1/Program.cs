d4d1673e1c66ba871aebeb15d7f252aeb0a58c32
nikola-golijanin <golijanin.nikola@outlook.com>
2024-05-09 23:17:47 +0200

    Chapter 1

---
+++ Chapter1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Chapter1;
+
+public static class ExtensionMethods
+{
+    public static (string Base, string Quote) SplitAt(this string s, int at)
+        => (s.Substring(0, at), s.Substring(at));
+
+    public static (string Base, string Quote) AsPair(this string pair)
+        => pair.SplitAt(3);
+
+    public static (IEnumerable<TSource> ConditionTrueCollection, IEnumerable<TSource> ConditionFalseCollection) Partition<TSource>(
+        this IEnumerable<TSource> ints, Func<TSource, bool> filter)
+    {
+        var enumerable = ints.ToList();
+        return (enumerable.Where(filter), enumerable.Where(i => !filter(i)));
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        //Two ways to split Currency string
+        var pair1 = "EURUSD".SplitAt(3);
+        Console.WriteLine(pair1.Base);
+        Console.WriteLine(pair1.Quote);
+
+        var pair2 = "EURUSD".AsPair();
+        Console.WriteLine(pair2.Base);
+        Console.WriteLine(pair2.Quote);
+
+        //create two collections of this range, one from numbers that completes this condition, other one from numbers that does not complete condition
+        //partition method used for that
+        var partition = Enumerable.Range(0, 10).Partition(i => i % 3 == 0);
+        foreach (var num in partition.ConditionTrueCollection)
+        {
+            Console.WriteLine(num);
+        }
+
+        foreach (var num in partition.ConditionFalseCollection)
+        {
+            Console.WriteLine(num);
+        }
+        
+        
+        //VAT Calculator example
+        var address = new Address("de");
+        var product = new Product("AAA", 12.5m, true);
+        var order = new Order(product, 2);
+
+        Console.WriteLine(VATCalculator.Vat(address, order));
+    }
+}
---
+++ Chapter1/VATCalculator.cs
@@ -0,0 +1,46 @@
+﻿namespace Chapter1;
+
+public static class VATCalculator
+{
+    public static decimal Vat(Address address, Order order)
+        => address switch
+        {
+            UsAddress(var state) => Vat(RateByState(state), order),
+            { Country: "de" } => DeVat(order),
+            { Country: var country } => Vat(RateByCountry(country), order),
+        };
+
+    static decimal RateByState(string state)
+        => state switch
+        {
+            "ca" => 0.1m,
+            "ma" => 0.0625m,
+            "ny" => 0.085m,
+            _ => throw new ArgumentException($"Missing rate for {state}")
+        };
+
+    private static decimal DeVat(Order order)
+        => order.NetPrice * (order.Product.IsFood ? 0.08m : 0.2m);
+
+    static decimal RateByCountry(string country)
+        => country switch
+        {
+            "it" => 0.22m,
+            "jp" => 0.08m,
+            _ => throw new ArgumentException($"Missing rate for {country}")
+        };
+
+    static decimal Vat(decimal rate, Order order)
+        => order.NetPrice * rate;
+   
+}
+
+public record Product(string Name, decimal Price, bool IsFood);
+
+public record Address(string Country);
+public record UsAddress(string State) : Address("us");
+
+public record Order(Product Product, int Quantity)
+{
+    public decimal NetPrice => Product.Price * Quantity;
+}
