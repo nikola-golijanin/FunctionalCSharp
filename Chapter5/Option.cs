15bdb1b0a78ee8f68bd3cdd5fb3ff0d6a09a183d
nikola-golijanin <golijanin.nikola@outlook.com>
2024-05-14 16:57:32 +0200

    Chapter5

---
+++ Chapter5/Option.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Unit = System.ValueTuple;
+
+namespace Chapter5;
+   using static F;
+
+   public static partial class F
+   {
+      // wrap the given value into a Some
+      public static Option<T> Some<T>([NotNull] T? value) // NotNull: `value` is guaranteed to never be null if the method returns without throwing an exception
+         => new(value ?? throw new ArgumentNullException(nameof(value)));
+
+      // the None value
+      public static NoneType None => default;
+   }
+
+   // a NoneType can be implicitely converted to an Option<T> for any type T
+   public struct NoneType { }
+
+   public struct Option<T> : IEquatable<NoneType>, IEquatable<Option<T>>
+   {
+      readonly T? value;
+      readonly bool isSome;
+      bool isNone => !isSome;
+
+      internal Option(T t) => (isSome, value) = (true, t);
+
+      public static implicit operator Option<T>(NoneType _) => default;
+
+      public static implicit operator Option<T>(T t)
+         => t is null ? None : new Option<T>(t);
+
+      public R Match<R>(Func<R> None, Func<T, R> Some)
+         => isSome ? Some(value!) : None();
+
+      public IEnumerable<T> AsEnumerable()
+      {
+         if (isSome) yield return value!;
+      }
+
+      public static bool operator true(Option<T> @this) => @this.isSome;
+      public static bool operator false(Option<T> @this) => @this.isNone;
+      public static Option<T> operator |(Option<T> l, Option<T> r) => l.isSome ? l : r;
+
+      // equality operators
+
+      public bool Equals(Option<T> other)
+         => this.isSome == other.isSome
+         && (this.isNone || this.value!.Equals(other.value));
+
+      public bool Equals(NoneType _) => isNone;
+
+      public static bool operator ==(Option<T> @this, Option<T> other) => @this.Equals(other);
+      public static bool operator !=(Option<T> @this, Option<T> other) => !(@this == other);
+
+      public override bool Equals(object? other)
+         => other is Option<T> option && this.Equals(option);
+
+      public override int GetHashCode()
+         => isNone ? 0 : value!.GetHashCode();
+
+      public override string ToString() => isSome ? $"Some({value})" : "None";
+   }
+
+   public static class OptionExt
+   {
+      public static Option<R> Apply<T, R>
+         (this Option<Func<T, R>> @this, Option<T> arg)
+         => @this.Match(
+            () => None,
+            (func) => arg.Match(
+               () => None,
+               (val) => Some(func(val))));
+
+
+
+      public static Option<R> Bind<T, R>
+         (this Option<T> optT, Func<T, Option<R>> f)
+          => optT.Match(
+             () => None,
+             (t) => f(t));
+
+
+      public static Option<R> Map<T, R>
+         (this NoneType _, Func<T, R> f)
+         => None;
+
+      public static Option<R> Map<T, R>
+         (this Option<T> optT, Func<T, R> f)
+         => optT.Match(
+            () => None,
+            (t) => Some(f(t)));
+
+
+   }
---
+++ Chapter5/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+using Chapter5;
+
+public static class Solutions
+{
+    // 1 Write a generic function that takes a string and parses it as a value of an enum. It
+    // should be usable as follows:
+
+    // OptHelper.Parse<DayOfWeek>("Friday") // => Some(DayOfWeek.Friday)
+    // OptHelper.Parse<DayOfWeek>("Freeday") // => None
+
+
+
+
+
+    // 2 Write a Lookup function that will take an IEnumerable and a predicate, and
+    // return the first element in the IEnumerable that matches the predicate, or None
+    // if no matching element is found. Write its signature in arrow notation:
+
+    // bool isOdd(int i) => i % 2 == 1;
+    // new List<int>().Lookup(isOdd) // => None
+    // new List<int> { 1 }.Lookup(isOdd) // => Some(1)
+
+    // Lookup : IEnumerable<T> -> (T -> bool) -> Option<T>
+    // solution: see OptHelper.Lookup
+
+
+
+
+    // 3 Write a type Email that wraps an underlying string, enforcing that it’s in a valid
+    // format. Ensure that you include the following:
+    // - A smart constructor
+    // - Implicit conversion to string, so that it can easily be used with the typical API
+    // for sending emails
+
+    public class Email
+    {
+        static readonly Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+        private string Value { get; }
+
+        private Email(string value) => Value = value;
+
+        public static Option<Email> Create(string s)
+           => regex.IsMatch(s)
+              ? F.Some(new Email(s))
+              : F.None;
+
+        public static implicit operator string(Email e)
+           => e.Value;
+    }
+}
+
+public static partial class OptHelper
+{
+    public static Option<T> Some<T>(T value) => new Option<T>(value);
+    public static NoneType None => default;
+
+    public static Option<int> Parse(string s)
+        => int.TryParse(s, out int result) ?
+            Some(result) :
+            None;
+
+    public static Option<string> Lookup(this NameValueCollection collection, string key)
+        => collection[key];
+
+    public static Option<T> Lookup<K, T>(this IDictionary<K, T> dict, K key)
+        => dict.TryGetValue(key, out T value) ? Some(value) : None;
+
+
+    public static Option<T> Lookup<T>(this IEnumerable<T> collection, Func<T, bool> _filter)
+    {
+        foreach (var item in collection)
+        {
+            if (_filter(item))
+                return Some(item);
+        };
+
+        return None;
+    }
+}
+
+
+public static class EnumHelper
+{
+    public static Option<T> Parse<T>(this string s) where T : struct
+       => Enum.TryParse(s, out T t) ? F.Some(t) : F.None;
+}
